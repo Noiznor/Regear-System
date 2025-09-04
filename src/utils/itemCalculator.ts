0238253d306c903f117c725aa50c731ff0a25234
Noiznor <124698043+Noiznor@users.noreply.github.com>
2025-09-04 16:10:26 +0800

    Update itemCalculator.ts

--- src/utils/itemCalculator.ts
+++ src/utils/itemCalculator.ts
@@ -13,22 +13,64 @@ export const calculateItemTotals = (thread: Thread): ItemCount => {
 
   Object.values(thread.roles).forEach(players => {
     players.forEach(player => {
-      addItems(player.selectedGear);
+      for (let i = 0; i < player.quantity; i++) {
+        addItems(player.selectedGear);
+      }
     });
   });
 
   return itemCounts;
 };
 
-export const getTierLimitedGear = (fullGear: GearPreset, tier: number): GearPreset => {
-  if (tier === 0 || tier === 1) {
+export const getTierLimitedGear = (fullGear: GearPreset, tier: number, role: string): GearPreset => {
+  if (tier === 1) {
+    // T1: Tank/Support/Healer get weapon + armor, DPS gets weapon only
+    if (role === 'dps') {
+      return {
+        weapon: fullGear.weapon,
+        headgear: '',
+        armor: '',
+        boots: '',
+        ...(fullGear.offhand && { offhand: fullGear.offhand })
+      };
+    } else {
+      return {
+        weapon: fullGear.weapon,
+        headgear: '',
+        armor: fullGear.armor,
+        boots: '',
+        ...(fullGear.offhand && { offhand: fullGear.offhand })
+      };
+    }
+  }
+  
+  if (tier === 2) {
+    // T2: Tank/Support/Healer get weapon + armor + boots, DPS gets weapon + armor
+    if (role === 'dps') {
+      return {
+        weapon: fullGear.weapon,
+        headgear: '',
+        armor: fullGear.armor,
+        boots: '',
+        ...(fullGear.offhand && { offhand: fullGear.offhand })
+      };
+    } else {
+      return {
+        weapon: fullGear.weapon,
+        headgear: '',
+        armor: fullGear.armor,
+        boots: fullGear.boots,
+        ...(fullGear.offhand && { offhand: fullGear.offhand })
+      };
+    }
+  }
+  
+  // T3 and T4 get full regear
+  if (tier === 3 || tier === 4) {
     return {
-      weapon: fullGear.weapon,
-      armor: fullGear.armor,
-      headgear: '',
-      boots: '',
-      ...(fullGear.offhand && { offhand: fullGear.offhand })
+      ...fullGear
     };
   }
+  
   return fullGear;
 };
